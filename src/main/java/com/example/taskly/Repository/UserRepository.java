d4da5b6824a627ef46c5553cb92a4932e14c734d
gresa13 <106274248+gresa13@users.noreply.github.com>
2025-05-18 20:48:27 +0200

    deleting
    delete

--- src/main/java/com/example/taskly/Repository/UserRepository.java
+++ src/main/java/com/example/taskly/Repository/UserRepository.java
@@ -4,7 +4,4 @@ import com.example.taskly.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
-    User findByUsername(String username);
 }
